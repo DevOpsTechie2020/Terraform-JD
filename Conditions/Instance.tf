6d2733ea3b551a413801ff90f99a78586626cd68
Pavan Bokka <rampavan.bokka@gmail.com>
2025-09-22 10:14:24 +0530

    code added

---
+++ Conditions/Instance.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "flm" {
+    tags= var.ec2_tags
+    ami = var.ami_id
+    instance_type = var.environment == "dev" ? "t3.micro" : "t3.small"
+    availability_zone = "us-east-1a"
+    vpc_security_group_ids = [aws_security_group.mysg.id]
+}
+resource "aws_security_group" "mysg" {
+    name = var.sg_name
+    description = var.sg_description
+
+    ingress {
+        from_port = var.from_port
+        to_port = var.to_port
+        protocol = "-1"
+        cidr_blocks = var.cidr_blocks
+    }
+    egress {
+        from_port = var.from_port
+        to_port = var.to_port
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
---
+++ Loops/variables.tf
@@ -0,0 +1,48 @@
+variable "ami_id" {
+    type = string
+    default = "ami-09c813fb71547fc4f"
+    description = "ami id of joindevops RHEL"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ec2_tags" {
+    type = map(string)
+    default = {
+        Name = "Terraform-Server"
+        Purpose = "Variables-demo"
+        Environment = "Dev"
+    }
+}
+
+variable "sg_name" {
+  default = "terraform-security-group"
+}
+
+variable "sg_description" {
+  default = "Allowing all ports from internet"
+}
+
+variable "from_port" {
+  default = 0
+}
+
+variable "to_port" {
+  type = number
+  default = 0
+}
+
+variable "cidr_blocks" {
+  type = list(string)
+  default = ["0.0.0.0/0"]
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "instances" {
+  default = ["mongodb", "redis", "mysql", "rabbitmq"]
+}
